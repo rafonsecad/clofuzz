3f54a545fb5f11fdcc327fa563f23bece17d4e7c
rafonsecad <rafd1788@gmail.com>
2025-06-09 18:07:36 -0500

    setup database connection

--- src/fuzz/backup.clj
+++ src/fuzz/backup.clj
@@ -1,8 +1,34 @@
 (ns fuzz.backup
+  (:require [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql])
   (:import (java.nio.file Files
                           Paths) 
            (java.net URI)
            (java.security MessageDigest)))
+
+(def db {:dbtype "sqlite" :dbname "clofuzz.db"})
+(def ds (jdbc/get-datasource db))
+
+(def scannings-table ["CREATE TABLE IF NOT EXISTS scannings (id INTEGER PRIMARY KEY,
+                                                             url TEXT,
+                                                             header TEXT,
+                                                             wordlist TEXT,
+                                                             wordlist_hash VARCHAR(64),
+                                                             method VARCHAR(10),
+                                                             match_codes TEXT,
+                                                             exclude_codes TEXT,
+                                                             filter_lengths TEXT,
+                                                             date TEXT,
+                                                             follow_redirects BOOLEAN,
+                                                             total INTEGER,
+                                                             processed_words INTEGER)"])
+
+(def matches-table ["CREATE TABLE IF NOT EXISTS matches (id INTEGER PRIMARY KEY,
+                                                         status INTEGER,
+                                                         word VARCHAR(30),
+                                                         length INTEGER,
+                                                         scanning_id INTEGER,
+                                                         FOREIGN KEY (scanning_id) REFERENCES scannings (id))"])
 
 (defn sha256sum [pathfile]
   (let [uri (new URI (str "file://" pathfile))
@@ -22,12 +48,25 @@
 (defn get-pwd []
   (System/getenv "PWD"))
 
-(defn data->file-edn [state stats matches]
-  (let [edn (:options state)]
-    (assoc edn :wordlist-hash (:wordlist-hash state))
-    (assoc edn :stats stats)
-    (assoc-in  edn [:stats :matches] matches)))
+(defn init-db []
+  (jdbc/execute! ds scannings-table)
+  (jdbc/execute! ds matches-table))
 
-(def FileBackup
+(defn data->table-edn [state stats matches]
+    [ (-> (:options state)
+          (assoc :id (:id state))
+          (assoc :date (:date state))
+          (assoc :wordlist-hash (:wordlist-hash state))
+          (assoc :total (:total stats))
+          (assoc :processed-words (:processed-words stats)))
+
+     (map #(assoc % :scanning-id (:id state)) matches)])
+
+(defn save-db [state stats matches]
+  (let [[scanning matches-db] (data->table-edn state stats matches)]
+    (sql/insert! ds :scannings scanning jdbc/snake-kebab-opts)
+    (sql/insert-multi! ds :matches matches-db jdbc/snake-kebab-opts)))
+
+(def DataBackup
   (reify BackupProtocol
     (save [_ state stats] nil)))
--- test/fuzz/backup_test.clj
+++ test/fuzz/backup_test.clj
@@ -1,49 +1,52 @@
 (ns fuzz.backup-test
   (:require [clojure.test :refer :all]
+            [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql]
             [fuzz.backup :as bk]))
 
 
-(deftest edn-transformation-tests
-  (testing "basic data transformation"
-    (is (bk/data->file-edn {:options {:url "http://test.com/FUZZ"
-                                      :header {}
-                                      :wordlist "test-resources/wordlist.txt"
-                                      :method "GET"
-                                      :match-codes [200]
-                                      :exclude-codes [ ]
-                                      :filter-lengths []
-                                      :follow-redirects false
-                                      }
-                            :wordlist-hash "1aadf7dafde5ca68f5e5160c9206f7be6f6fc701775cdea30ba01bbb6d8db8ad"} 
-                           {:total 43007 :processed-words 1000}
-                           [{:status 200 :word "index.html" :length 1000}])
-        {:url "http://test.com/FUZZ"
-         :header {}
-         :wordlist "test-resources/wordlist.txt"
-         :wordlist-hash "1aadf7dafde5ca68f5e5160c9206f7be6f6fc701775cdea30ba01bbb6d8db8ad"
-         :method "GET"
-         :match-codes [200]
-         :exclude-codes [ ]
-         :filter-lengths []
-         :follow-redirects false
-         :stats {:total 43007
-                 :processed-words 1000
-                 :matches [{:status 200 :word "index.html" :length 1000}]}})))
+(defn with-database [test-fn]
+  (bk/init-db)
+  (test-fn)
+  (jdbc/execute! bk/ds ["DROP TABLE matches"])
+  (jdbc/execute! bk/ds ["DROP TABLE scannings"]))
 
-(deftest save-edn-test
-  (testing "saving edn"
-    (is (slurp ".clofuzz/test.com/2025-05-31-1")
-        (bk/save {:options {:url "http://test.com/FUZZ"
-                            :header {}
-                            :wordlist "test-resources/wordlist.txt"
-                            :method "GET"
-                            :match-codes [200]
-                            :exclude-codes [ ]
-                            :filter-lengths []
-                            :follow-redirects false
-                            }
-                  :wordlist-hash "1aadf7dafde5ca68f5e5160c9206f7be6f6fc701775cdea30ba01bbb6d8db8ad"} 
-                 {:total 43007 :processed-words 1000}
-                 [{:status 200 :word "index.html" :length 1000}]))))
+(use-fixtures :once with-database)
+
+(deftest save-state-test
+  (testing "save data in db"
+    (bk/save-db {:options {:url "http://test.com/FUZZ"
+                                              :header {}
+                                              :wordlist "test-resources/wordlist.txt"
+                                              :method "GET"
+                                              :match-codes [200]
+                                              :exclude-codes [ ]
+                                              :filter-lengths []
+                                              :follow-redirects false
+                                              }
+                                   :wordlist-hash "1aadf7dafde5ca68f5e5160c9206f7be6f6fc701775cdea30ba01bbb6d8db8ad"
+                                   :id 1
+                                   :date "2025-05-31"} 
+                                   {:total 43007 :processed-words 1000}
+                                   [{:status 200 :word "index.html" :length 1000}])
+    
+    (is (= (sql/get-by-id bk/ds :scannings 1 next.jdbc/unqualified-snake-kebab-opts)
+           {:url "http://test.com/FUZZ"
+            :header "{}" 
+            :wordlist "test-resources/wordlist.txt"
+            :wordlist-hash "1aadf7dafde5ca68f5e5160c9206f7be6f6fc701775cdea30ba01bbb6d8db8ad"
+            :method "GET"
+            :match-codes "[200]" 
+            :exclude-codes "[]" 
+            :filter-lengths "[]" 
+            :follow-redirects 0
+            :id 1
+            :date "2025-05-31"
+            :total 43007
+            :processed-words 1000}))
+    
+    (is (= (sql/find-by-keys bk/ds :matches {:scanning-id 1} next.jdbc/unqualified-snake-kebab-opts)
+           [{:id 1 :status 200 :word "index.html" :length 1000 :scanning-id 1}]))))
+
 
 ;(run-tests)
